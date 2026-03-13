6492e54c7c5428c696063e7263b0e703a6e94d1f
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2026-03-13 22:01:24 +0200

    Refactor noteService

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -3,8 +3,7 @@ import type { Note } from '../types/note';
 
 axios.defaults.baseURL = 'https://notehub-public.goit.study/api/auth/notes/';
 axios.defaults.headers.common['Authorization'] =
-  import.meta.env.VITE_NOTEHUB_TOKEN;
-axios.defaults.params = {};
+  `Bearer ${import.meta.env.VITE_NOTEHUB_TOKEN}`;
 
 interface NotehubResponse {
   notes: Note[];
@@ -12,17 +11,21 @@ interface NotehubResponse {
 }
 
 export const fetchNotes = async (query: string, page: number) => {
-  const response = await axios.get<NotehubResponse>(
-    `search?query=${query}&page=${page}&perPage=${12}`
-  );
+  const response = await axios.get<NotehubResponse>(`search`, {
+    params: {
+      query,
+      page,
+      perPage: 12,
+    },
+  });
   return response.data;
 };
 
 export const createNote = async (title: string, content: string, tag: string) =>
-  await axios.post(``, {
+  await axios.post('', {
     title,
     content,
     tag,
   });
 
-export const deleteNote = async (id: string) => await axios.delete(`${id}`);
+export const deleteNote = async (id: string) => await axios.delete(id);
