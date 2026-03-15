b5c9439dffac3b221a7eb81c3e57388c75ed660e
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2026-03-15 12:22:13 +0200

    Add folder components and change path to App

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import css from './App.module.css';
+
+function App() {
+  return (
+    <div className={css.app}>
+      <header className={css.toolbar}>
+        {/* Компонент SearchBox */}
+        {/* Пагінація */}
+        {/* Кнопка створення нотатки */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import App from './App/App.tsx';
+import App from './components/App/App.tsx';
 
 import 'modern-normalize';
 
