d210d393633ebd8909adf930bdeb203b04fe4b6d
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2026-03-15 19:51:07 +0200

    Add ReactQueryDevtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import App from './components/App/App.tsx';
 
@@ -10,5 +11,6 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <App />
+    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
 );
