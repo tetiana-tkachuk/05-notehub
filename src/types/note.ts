94038c9234c00866a7c0b7e1319a53e358e63917
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2026-03-16 13:39:45 +0200

    Change NoteTag interface to type

--- src/types/note.ts
+++ src/types/note.ts
@@ -7,6 +7,4 @@ export interface Note {
   tag: string;
 }
 
-export interface NoteTag {
-  tag: 'Todo' | 'Work' | 'Personal' | 'Meeting' | 'Shopping';
-}
+export type NoteTag = 'Todo' | 'Work' | 'Personal' | 'Meeting' | 'Shopping';
