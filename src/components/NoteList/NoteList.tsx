f6fec386fc18682761767ae7c85c51825f5ab827
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2026-03-16 19:55:54 +0200

    Implement note deletion

--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -1,26 +1,52 @@
-import type { Note } from '../../types/note';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import toast, { Toaster } from 'react-hot-toast';
+
 import css from './NoteList.module.css';
+import type { Note } from '../../types/note';
+import { deleteNote } from '../../services/noteService';
 
 interface NoteListProps {
   notes: Note[];
 }
 
 export default function NoteList({ notes }: NoteListProps) {
-  console.log(notes);
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: async (id: string) => {
+      return await deleteNote(id);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['notes'] });
+    },
+    onError: () => {
+      toast.error('Something went wrong, please try again...');
+    },
+  });
+
   return (
-    <ul className={css.list}>
-      {notes.map(note => {
-        return (
-          <li className={css.listItem} key={note.id}>
-            <h2 className={css.title}>{note.title}</h2>
-            <p className={css.content}>{note.content}</p>
-            <div className={css.footer}>
-              <span className={css.tag}>{note.tag}</span>
-              <button className={css.button}>Delete</button>
-            </div>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <ul className={css.list}>
+        {notes.map(note => {
+          return (
+            <li className={css.listItem} key={note.id}>
+              <h2 className={css.title}>{note.title}</h2>
+              <p className={css.content}>{note.content}</p>
+              <div className={css.footer}>
+                <span className={css.tag}>{note.tag}</span>
+                <button
+                  className={css.button}
+                  type="button"
+                  onClick={() => mutation.mutate(note.id)}
+                >
+                  Delete
+                </button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+      <Toaster />
+    </>
   );
 }
